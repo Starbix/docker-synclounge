a89fa448c328fe1dda753a6820dbff28f2d6c3cd
Sam <sam_cm_to@hotmail.com>
2018-07-20 03:30:43 +1000

    Rebuild webapp if autoJoin Env variable exists

--- dev/run.sh
+++ dev/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 cd /opt/synclounge
+if [ "autoJoin" == "true" ]; then
+  echo 'Rebuilding for autojoin support'
+  npm run build
+else
+  echo 'Not rebuilding the app'
+fi
 export accessUrl=http://$DOMAIN; npm run server & node webapp.js
